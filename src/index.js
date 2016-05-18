6c3e6dc8ad08768cca8b4304093d2abd1bdfaa18
Sean Mullen <srmullen13@gmail.com>
2016-05-18 17:53:09 -0600

    added react-codemirror

--- src/index.js
+++ src/index.js
@@ -3,9 +3,13 @@ import ReactDOM from "react-dom";
 import {parser} from "boethius-lang";
 import superSaw from "./synths/superSaw";
 import PlayButton from "./PlayButton";
+import CodeMirror from "react-codemirror";
 
 ReactDOM.render(
-    <PlayButton />,
+    <div>
+        <PlayButton />
+        <CodeMirror value={"hello"} options={{lineNumbers: true}} />
+    </div>,
     document.getElementById("root")
 )
 
